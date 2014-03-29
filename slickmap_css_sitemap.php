99d922e3bd9bcc5a5183eb739bff0b3ef4ba4a3b
Chad A. Carino <chadc@tekserve.com>
2014-03-28 21:34:56 -0400

    Settings for style array created; all save but padding array.

--- slickmap_css_sitemap.php
+++ slickmap_css_sitemap.php
@@ -67,23 +67,6 @@ function slickmap_css_sitemap_menu_content($active_tab = '') {
             	settings_fields( 'slickmap_css_sitemap_menu_general' );
 				do_settings_sections( 'slickmap_css_sitemap_menu_general' ); 
             }
-            // settings_fields( 'slickmap_css_sitemap_colors_home' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_colors_home' ); 
-//             <?php settings_fields( 'slickmap_css_sitemap_colors_level1' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_colors_level1' );
-//             <?php settings_fields( 'slickmap_css_sitemap_colors_level2' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_colors_level2' ); 
-//             <?php settings_fields( 'slickmap_css_sitemap_colors_level3' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_colors_level3' );
-//              
-//             <?php settings_fields( 'slickmap_css_sitemap_text_home' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_text_home' ); 
-//             <?php settings_fields( 'slickmap_css_sitemap_text_level1' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_text_level1' ); 
-//             <?php settings_fields( 'slickmap_css_sitemap_text_level2' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_text_level2' ); 
-//             <?php settings_fields( 'slickmap_css_sitemap_text_level3' ); 
-//             <?php do_settings_sections( 'slickmap_css_sitemap_text_level3' ); 
            
         	submit_button(); ?>
              
@@ -278,6 +261,7 @@ function slickmap_css_sitemap_general_padding_callback() {
 	echo "<p>Bottom: <input size='10' name='slickmap_css_sitemap_general_padding_bottom' id='slickmap_css_sitemap_general_padding_bottom' type='text' value='" . $padding['bottom'] . "' /></p>";
 	echo "<p>Right: <input size='10' name='slickmap_css_sitemap_general_padding_right' id='slickmap_css_sitemap_general_padding_right' type='text' value='" . $padding['right'] . "' /></p>";
 	echo "<p>Left: <input size='10' name='slickmap_css_sitemap_general_padding_left' id='slickmap_css_sitemap_general_padding_left' type='text' value='" . $padding['left'] . "' /></p>";
+	update_option( 'slickmap_css_sitemap_general_padding', $padding );
 }
 
 function slickmap_css_sitemap_home_bgcolor_callback() {
@@ -543,61 +527,217 @@ function slickmap_css_sitemap_shortcode( $atts, $content = null ) {
 	// attributes allow for manual override of color settings... just in case?
 	extract( shortcode_atts(
 		array(
-			'color1' => '',
-			'color2' => '',
-			'color3' => '',
+			'color1' 	=>	'',
+			'color2' 	=>	'',
+			'color3' 	=>	'',
+			'columns'	=>	'7',
 		), $atts )
 	);
 	//fetch options, store non-defaults as css strings in arrays
-		//level 1 - #primaryNav li a 
+		$style_rules = array (
+			'target1'			=>	array ('selector'	=>	'.slickmap #primaryNav li a'),
+			'target1_before'	=>	array ('selector'	=>	'.slickmap #primaryNav li a:link::before, .slickmap #primaryNav li a:visited::before'),
+			'target1_hover'		=>	array ('selector'	=>	'.slickmap #primaryNav li a:hover'),
+			'target2'			=>	array ('selector'	=>	'.slickmap #primaryNav li li a'),
+			'target2_before'	=>	array ('selector'	=>	'.slickmap #primaryNav li li a:link::before, .slickmap #primaryNav li li a:visited::before'),
+			'target2_hover'		=>	array ('selector'	=>	'.slickmap #primaryNav li li a:hover'),
+			'target3'			=>	array ('selector'	=>	'.slickmap #primaryNav li li li a'),
+			'target3_before'	=>	array ('selector'	=>	'.slickmap #primaryNav li li li a:link::before, .slickmap #primaryNav li li li a:visited::before'),
+			'target3_hover'		=>	array ('selector'	=>	'.slickmap #primaryNav li li li a:hover'),
+			'target4'			=>	array ('selector'	=>	'.slickmap #primaryNav #home a'),
+			'target4_before'	=>	array ('selector'	=>	'.slickmap #primaryNav #home a:link::before, .slickmap #primaryNav #home a:visited::before'),
+			'target4_hover'		=>	array ('selector'	=>	'.slickmap #primaryNav #home a:hover'),
+			'target5'			=>	array ('selector'	=>	'.slickmap #primaryNav'),
+			'target6'			=>	array ('selector'	=>	'.slickmap #primaryNav li'),
+		);
+		//level 1 - #primaryNav li a
 			//background: $color; (if background-color≠#c3eafb)
+			if( get_option( 'slickmap_css_sitemap_level1_bgcolor' ) != '#c3eafb' ) {
+				$style_rules['target1']['background-color'] = get_option( 'slickmap_css_sitemap_level1_bgcolor' );
+			}
 			//border: 2px solid $color; (if border-color≠#b5d9ea)
+			if( get_option( 'slickmap_css_sitemap_level1_bordercolor' ) != '#b5d9ea' ) {
+				$style_rules['target1']['border'] = '2px solid ' . get_option( 'slickmap_css_sitemap_level1_bordercolor' );
+			}
 			//title text color: $color; (if color≠black)
+			if( get_option( 'slickmap_css_sitemap_level1_title_text_color' ) != '#000000' ) {
+				$style_rules['target1']['color'] = get_option( 'slickmap_css_sitemap_level1_title_text_color' );
+			}
 			//title font-size: $size; (if font-size≠14px)
+			if( get_option( 'slickmap_css_sitemap_level1_title_text_size' ) != '14px' ) {
+				$style_rules['target1']['font-size'] = get_option( 'slickmap_css_sitemap_level1_title_text_size' );
+			}
 			//(-webkit-,-o-,-moz-)border-radius: $radius; (if border-radius≠5px)
+			if( get_option( 'slickmap_css_sitemap_general_radius' ) != '5px' ) {
+				$style_rules['target1']['-webkit-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target1']['-moz-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target1']['-o-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target1']['border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+			}
 			//background hover color - #primaryNav li a:hover { background-color: $color; } (if background-color≠#e2f4fd)
+			if( get_option( 'slickmap_css_sitemap_level1_bgcolor_hover' ) != '#e2f4fd' ) {
+				$style_rules['target1_hover']['background-color'] =  get_option( 'slickmap_css_sitemap_level1_bgcolor_hover' );
+			}
 			//border hover color - #primaryNav li a:hover { border-color: $color; } (if border-color≠#97bdcf)
+			if( get_option( 'slickmap_css_sitemap_level1_bordercolor_hover' ) != '#97bdcf' ) {
+				$style_rules['target1_hover']['border-color'] =  get_option( 'slickmap_css_sitemap_level1_bordercolor_hover' );
+			}
 			//path text color #primaryNav li a:link:before, #primaryNav li a:visited:before { color: $color; } (if color≠#78a9c0)
+			if( get_option( 'slickmap_css_sitemap_level1_path_text_color' ) != '#78a9c0' ) {
+				$style_rules['target1_before']['color'] =  get_option( 'slickmap_css_sitemap_level1_path_text_color' );
+			}
 			//path font-size #primaryNav li a:link:before, #primaryNav li a:visited:before { font-size: $size; } (if font-size≠10px)
-			//show/hide gradient (if hide) background-image: none;
+			if( get_option( 'slickmap_css_sitemap_level1_path_text_size' ) != '10px' ) {
+				$style_rules['target1_before']['font-size'] =  get_option( 'slickmap_css_sitemap_level1_path_text_size' );
+			}
+
 		//level 2 - #primaryNav li li a 
 			//background: $color; (if background-color≠#cee3ac)
+			if( get_option( 'slickmap_css_sitemap_level2_bgcolor' ) != '#cee3ac' ) {
+				$style_rules['target2']['background-color'] = get_option( 'slickmap_css_sitemap_level2_bgcolor' );
+			}
 			//border: 2px solid $color; (if border-color≠#b8da83)
+			if( get_option( 'slickmap_css_sitemap_level2_bordercolor' ) != '#b8da83' ) {
+				$style_rules['target2']['border'] = '2px solid ' . get_option( 'slickmap_css_sitemap_level2_bordercolor' );
+			}
 			//title text color: $color; (if color≠black)
+			if( get_option( 'slickmap_css_sitemap_level2_title_text_color' ) != '#000000' ) {
+				$style_rules['target2']['color'] = get_option( 'slickmap_css_sitemap_level2_title_text_color' );
+			}
 			//title font-size: $size; (if font-size≠14px)
+			if( get_option( 'slickmap_css_sitemap_level2_title_text_size' ) != '14px' ) {
+				$style_rules['target2']['font-size'] = get_option( 'slickmap_css_sitemap_level2_title_text_size' );
+			}
 			//(-webkit-,-o-,-moz-)border-radius: $radius; (if border-radius≠5px)
+			if( get_option( 'slickmap_css_sitemap_general_radius' ) != '5px' ) {
+				$style_rules['target2']['-webkit-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target2']['-moz-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target2']['-o-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target2']['border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+			}
 			//background hover color - #primaryNav li li a:hover { background-color: $color; } (if background-color≠#e7f1d7)
+			if( get_option( 'slickmap_css_sitemap_level2_bgcolor_hover' ) != '#e7f1d7' ) {
+				$style_rules['target2_hover']['background-color'] =  get_option( 'slickmap_css_sitemap_level2_bgcolor_hover' );
+			}
 			//border hover color - #primaryNav li li a:hover { border-color: $color; } (if border-color≠#94b75f)
+			if( get_option( 'slickmap_css_sitemap_level2_bordercolor_hover' ) != '#94b75f' ) {
+				$style_rules['target2_hover']['border-color'] =  get_option( 'slickmap_css_sitemap_level2_bordercolor_hover' );
+			}
 			//path text color #primaryNav li li a:link:before, #primaryNav li li a:visited:before { color: $color; } (if color≠#8faf5c)
+			if( get_option( 'slickmap_css_sitemap_level2_path_text_color' ) != '#8faf5c' ) {
+				$style_rules['target2_before']['color'] =  get_option( 'slickmap_css_sitemap_level2_path_text_color' );
+			}
 			//path font-size #primaryNav li li a:link:before, #primaryNav li li a:visited:before { font-size: $size; } (if font-size≠10px)
-			//show/hide gradient (if hide) background-image: none;
+			if( get_option( 'slickmap_css_sitemap_level2_path_text_size' ) != '10px' ) {
+				$style_rules['target2_before']['font-size'] =  get_option( 'slickmap_css_sitemap_level2_path_text_size' );
+			}
+
 		//level 3 - #primaryNav li li li a 
 			//background: $color; (if background-color≠#fff7aa)
+			if( get_option( 'slickmap_css_sitemap_level3_bgcolor' ) != '#fff7aa' ) {
+				$style_rules['target3']['background-color'] = get_option( 'slickmap_css_sitemap_level3_bgcolor' );
+			}
 			//border: 2px solid $color; (if border-color≠#e3ca4b)
+			if( get_option( 'slickmap_css_sitemap_level3_bordercolor' ) != '#e3ca4b' ) {
+				$style_rules['target3']['border'] = '2px solid ' . get_option( 'slickmap_css_sitemap_level3_bordercolor' );
+			}
 			//title text color: $color; (if color≠black)
+			if( get_option( 'slickmap_css_sitemap_level3_title_text_color' ) != '#000000' ) {
+				$style_rules['target3']['color'] = get_option( 'slickmap_css_sitemap_level3_title_text_color' );
+			}
 			//title font-size: $size; (if font-size≠12px)
+			if( get_option( 'slickmap_css_sitemap_level3_title_text_size' ) != '12px' ) {
+				$style_rules['target3']['font-size'] = get_option( 'slickmap_css_sitemap_level3_title_text_size' );
+			}
 			//(-webkit-,-o-,-moz-)border-radius: $radius; (if border-radius≠5px)
+			if( get_option( 'slickmap_css_sitemap_general_radius' ) != '5px' ) {
+				$style_rules['target3']['-webkit-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target3']['-moz-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target3']['-o-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target3']['border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+			}
 			//background hover color - #primaryNav li li li a:hover { background-color: $color; } (if background-color≠#fffce5)
+			if( get_option( 'slickmap_css_sitemap_level3_bgcolor_hover' ) != '#fffce5' ) {
+				$style_rules['target3_hover']['background-color'] =  get_option( 'slickmap_css_sitemap_level3_bgcolor_hover' );
+			}
 			//border hover color - #primaryNav li li li a:hover { border-color: $color; } (if border-color≠#d1b62c)
+			if( get_option( 'slickmap_css_sitemap_level3_bordercolor_hover' ) != '#d1b62c' ) {
+				$style_rules['target3_hover']['border-color'] =  get_option( 'slickmap_css_sitemap_level3_bordercolor_hover' );
+			}
 			//path text color #primaryNav li li li a:link:before, #primaryNav li li li a:visited:before { color: $color; } (if color≠#ccae14)
+			if( get_option( 'slickmap_css_sitemap_level3_path_text_color' ) != '#ccae14' ) {
+				$style_rules['target3_before']['color'] =  get_option( 'slickmap_css_sitemap_level3_path_text_color' );
+			}
 			//path font-size #primaryNav li li li a:link:before, #primaryNav li li li a:visited:before { font-size: $size; } (if font-size≠9px)
-			//show/hide gradient (if hide) background-image: none;
+			if( get_option( 'slickmap_css_sitemap_level3_path_text_size' ) != '9px' ) {
+				$style_rules['target3_before']['font-size'] =  get_option( 'slickmap_css_sitemap_level3_path_text_size' );
+			}
+
 		//home - #primaryNav #home a
 			//background: $color; (if background-color≠#c3eafb)
+			if( get_option( 'slickmap_css_sitemap_home_bgcolor' ) != '#c3eafb' ) {
+				$style_rules['target4']['background-color'] = get_option( 'slickmap_css_sitemap_home_bgcolor' );
+			}
 			//border: 2px solid $color; (if border-color≠#b5d9ea)
+			if( get_option( 'slickmap_css_sitemap_home_bordercolor' ) != '#b5d9ea' ) {
+				$style_rules['target4']['border'] = '2px solid ' . get_option( 'slickmap_css_sitemap_home_bordercolor' );
+			}
 			//title text color: $color; (if color≠black)
+			if( get_option( 'slickmap_css_sitemap_home_title_text_color' ) != '#000000' ) {
+				$style_rules['target4']['color'] = get_option( 'slickmap_css_sitemap_home_title_text_color' );
+			}
 			//title font-size: $size; (if font-size≠14px)
+			if( get_option( 'slickmap_css_sitemap_home_title_text_size' ) != '14px' ) {
+				$style_rules['target4']['font-size'] = get_option( 'slickmap_css_sitemap_home_title_text_size' );
+			}
 			//(-webkit-,-o-,-moz-)border-radius: $radius; (if border-radius≠5px)
-			//background hover color - #primaryNav #home a:hover { background-color: $color; } (if background-color≠#e2f4fd)
-			//border hover color - #primaryNav #home a:hover { border-color: $color; } (if border-color≠#97bdcf)
-			//path text color #primaryNav #home a:link:before, #primaryNav #home a:visited:before { color: $color; } (if color≠#78a9c0)
-			//path font-size #primaryNav #home a:link:before, #primaryNav #home a:visited:before { font-size: $size; } (if font-size≠10px)
-			//show/hide gradient (if hide) background-image: none;
+			if( get_option( 'slickmap_css_sitemap_general_radius' ) != '5px' ) {
+				$style_rules['target4']['-webkit-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target4']['-moz-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target4']['-o-border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+				$style_rules['target4']['border-radius'] = get_option( 'slickmap_css_sitemap_general_radius' );
+			}
+			//background hover color - #primaryNav li a:hover { background-color: $color; } (if background-color≠#e2f4fd)
+			if( get_option( 'slickmap_css_sitemap_level1_bgcolor_hover' ) != '#e2f4fd' ) {
+				$style_rules['target4_hover']['background-color'] =  get_option( 'slickmap_css_sitemap_level1_bgcolor_hover' );
+			}
+			//border hover color - #primaryNav li a:hover { border-color: $color; } (if border-color≠#97bdcf)
+			if( get_option( 'slickmap_css_sitemap_level1_bordercolor_hover' ) != '#97bdcf' ) {
+				$style_rules['target4_hover']['border-color'] =  get_option( 'slickmap_css_sitemap_level1_bordercolor_hover' );
+			}
+			//path text color #primaryNav li a:link:before, #primaryNav li a:visited:before { color: $color; } (if color≠#78a9c0)
+			if( get_option( 'slickmap_css_sitemap_home_path_text_color' ) != '#78a9c0' ) {
+				$style_rules['target4_before']['color'] =  get_option( 'slickmap_css_sitemap_home_path_text_color' );
+			}
+			//path font-size #primaryNav li a:link:before, #primaryNav li a:visited:before { font-size: $size; } (if font-size≠10px)
+			if( get_option( 'slickmap_css_sitemap_home_path_text_size' ) != '10px' ) {
+				$style_rules['target4_before']['font-size'] =  get_option( 'slickmap_css_sitemap_home_path_text_size' );
+			}
 	//check if atts included any colors, change any array strings that they would override
 		//if so, override array string for
 			//#primaryNav li a { background-color: $color1;}
 			//#primaryNav li li a { background-color: $color2;}
 			//#primaryNav li li li a { background-color: $color3;}
+	//add global rules for entire sitemap to array
+	//show/hide gradient (if hide) background-image: none;
+	if( get_option( 'slickmap_css_sitemap_general_gradient' ) == 'hide' ) {
+		$style_rules['target1']['background-image'] =  'none !important';
+	}
+	//padding (if≠10px 0) $padding;
+	$padding_array = get_option( 'slickmap_css_sitemap_general_padding' );
+	$padding_string = $padding_array['top'] . ' ' . $padding_array['right'] . ' ' . $padding_array['bottom'] . ' ' . $padding_array['left'];
+	if( $padding_string != '10px 0px 10px 0px' ) {
+		$style_rules['target1']['padding'] =  $padding_string . ' !important';
+	}
+	//font stack for sitemap
+	if( get_option( 'slickmap_css_sitemap_general_font_family' ) != 'Gotham, Helvetica, Arial, sans-serif' ) {
+		$style_rules['target5']['font-family'] =  get_option( 'slickmap_css_sitemap_general_font_family' ) . ' !important';
+	}
+	//size columns based on number of columns; 7 by default in this implementation
+	if( $atts['columns'] != 7 ) {
+		$style_rules['target6']['width'] =  intval( 100 / intval( $atts['columns'] ) ). ' !important';
+	}
+	
 	//create style rules to override included stylesheet
 	$style = '<style>';
 	//$style .= foreach css selector in array (#primaryNav li a; #primaryNav li a:hover; #primaryNav li a:link:before, #primaryNav li a:visited:before; #primaryNav li li a; #primaryNav li li a:hover; #primaryNav li li a:link:before, #primaryNav li li a:visited:before; #primaryNav li li li a; #primaryNav li li li a:hover; #primaryNav li li li a:link:before, #primaryNav li li li a:visited:before; #primaryNav #home a; #primaryNav #home a:hover; #primaryNav #home a:link:before, #primaryNav #home a:visited:before)
@@ -619,6 +759,9 @@ function slickmap_css_sitemap_shortcode( $atts, $content = null ) {
 		jQuery("#primaryNav>li:first-child").attr("id", "home");
 	});
 	</script>' . $content;
+	
+	print_r( $style_rules );
+	
 	//concatenate all content for output
 	//style tag here
 	$sitemap = $style;
@@ -629,6 +772,7 @@ function slickmap_css_sitemap_shortcode( $atts, $content = null ) {
 	//final output of sitemap
     return $sitemap;
 }
+
 add_shortcode( 'slickmap', 'slickmap_css_sitemap_shortcode' );
 
 //include slickmap css; sets defaults and display style
